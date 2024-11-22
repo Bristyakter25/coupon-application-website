a267231c9e46ec143bc69b3b1c295cfb036aa5c4
Bristyakter25 <sabihaakterbristy@gmail.com>
2024-11-22 23:06:13 +0600

    Added ErrorPage

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -5,7 +5,7 @@ const ErrorPage = () => {
         <div>
             <h2 className='text-2xl'>Not Found Page!!!</h2>
             <div className="btn text-2xl">
-            <NavLink  to='/'>Go to Back.</NavLink>
+            <NavLink  to='/'>Go to Back in the Website.</NavLink>
             </div>
            
         </div>
